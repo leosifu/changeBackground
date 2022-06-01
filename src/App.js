e7cbac2f194da2e711a17f7dcd517a4344400e68
Leonardo <leonardo.sifuentes@usach.cl>
2022-05-31 21:52:59 -0400

    improve code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {useState, useEffect, } from 'react';
+import {useState, } from 'react';
 
 import './App.css';
 
 import { Container, Row, Col, Button, } from 'react-bootstrap';
 
 const colors = {
-  0: 'green',
-  1: 'red',
-  2: 'blue',
-  3: 'yellow',
-  4: 'black'
+  0: 'backgroundGreen',
+  1: 'backgroundRed',
+  2: 'backgroundBlue',
+  3: 'backgroundYellow',
+  4: 'backgroundBlack'
 }
 
 function App() {
@@ -19,21 +19,21 @@ function App() {
   const changeColor = (color) => setCurrentColor(currentColorState => color);
 
   return (
-    <div className="App" style={{ backgroundColor: colors[currentColor], minHeight: '100vh' }}>
+    <div className={`App ${colors[currentColor]}`}>
       <div className="App-header">
       <Container>
         <Row>
-          <Col xs={12} md={6} lg={3}>
-            <Button variant="danger" onClick={() => changeColor(1)}>Warning</Button>
+          <Col xs={12} md={6} lg={3} className="buttonChangeColor" >
+            <Button variant="danger" onClick={() => changeColor(1)}>Fondo rojo</Button>
           </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Button variant="primary" onClick={() => changeColor(2)}>Primary</Button>
+          <Col xs={12} md={6} lg={3} className="buttonChangeColor" >
+            <Button variant="primary" onClick={() => changeColor(2)}>Fondo azul</Button>
           </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Button variant="warning" onClick={() => changeColor(3)}>Warning</Button>
+          <Col xs={12} md={6} lg={3} className="buttonChangeColor" >
+            <Button variant="warning" onClick={() => changeColor(3)}>Fondo amarillo</Button>
           </Col>
-          <Col xs={12} md={6} lg={3}>
-            <Button variant="dark" onClick={() => changeColor(4)}>Dark</Button>
+          <Col xs={12} md={6} lg={3} className="buttonChangeColor" >
+            <Button variant="dark" onClick={() => changeColor(4)}>Fondo negro</Button>
           </Col>
         </Row>
       </Container>
