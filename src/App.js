fc9b9d4a6384ab2d8ff144e625418c52e71f9cdc
Leonardo <leonardo.sifuentes@usach.cl>
2022-05-31 21:18:22 -0400

    change color of background by click on buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import {useState, useEffect, } from 'react';
+
 import './App.css';
 
+import { Container, Row, Col, Button, } from 'react-bootstrap';
+
+const colors = {
+  0: 'green',
+  1: 'red',
+  2: 'blue',
+  3: 'yellow',
+  4: 'black'
+}
+
 function App() {
+
+  const [currentColor, setCurrentColor] = useState(0);
+
+  const changeColor = (color) => setCurrentColor(currentColorState => color);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: colors[currentColor], minHeight: '100vh' }}>
+      <div className="App-header">
+      <Container>
+        <Row>
+          <Col xs={12} md={6} lg={3}>
+            <Button variant="danger" onClick={() => changeColor(1)}>Warning</Button>
+          </Col>
+          <Col xs={12} md={6} lg={3}>
+            <Button variant="primary" onClick={() => changeColor(2)}>Primary</Button>
+          </Col>
+          <Col xs={12} md={6} lg={3}>
+            <Button variant="warning" onClick={() => changeColor(3)}>Warning</Button>
+          </Col>
+          <Col xs={12} md={6} lg={3}>
+            <Button variant="dark" onClick={() => changeColor(4)}>Dark</Button>
+          </Col>
+        </Row>
+      </Container>
+      </div>
     </div>
   );
 }
